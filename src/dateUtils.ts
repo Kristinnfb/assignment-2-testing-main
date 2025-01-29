3f547951e29a2a44cd3f878a7b1ba49c9a2d63a2
Kristinn F Birgisson <88903567+Kristinnfb@users.noreply.github.com>
2025-01-29 14:10:44 +0000

    Added Type definition to arguments and functions in src/dateUtils.ts

---
+++ src/dateUtils.ts
@@ -0,0 +1,22 @@
+import moment from "moment";
+import { DATE_UNIT_TYPES } from "./constants";
+
+export function getCurrentYear() {
+  return moment().year();
+}
+
+export function add(date: Date, number: number, type = DATE_UNIT_TYPES.DAYS) : Date {
+  return moment(date).add(number, type).toDate();
+}
+
+export function isWithinRange(date: Date, from: string, to: string) : boolean {
+  return moment(date).isBetween(from, to);
+}
+
+export function isDateBefore(date: Date, compareDate: Date) : boolean {
+  return moment(date).isBefore(compareDate);
+}
+
+export function isSameDay(date: Date, compareDate: Date) : boolean {
+  return moment(date).isSame(compareDate);
+}
