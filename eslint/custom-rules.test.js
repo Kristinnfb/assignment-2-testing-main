f37889e991ed5b3ab37518c8d1a74fe5fa7e8dde
Kristinn F Birgisson <88903567+Kristinnfb@users.noreply.github.com>
2025-02-18 23:59:05 +0000

    Added esLint and prettier

--- eslint/custom-rules.test.js
+++ eslint/custom-rules.test.js
@@ -1,20 +1,18 @@
-const { RuleTester } = require("eslint");
+// eslint/custom-rules.test.js
+import { RuleTester } from 'eslint';
+import customRules from './custom-rules.js';
 
-const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 2020 } });
+const ruleTester = new RuleTester({
+  parserOptions: { ecmaVersion: 2022 }
+});
 
-// Test camelcase-functions rule
-ruleTester.run(
-  "camelcase-functions",
-  require("./custom-rules").rules["camelcase-functions"],
-  {
-    valid: ["function myFunction() {}"],
-    invalid: [
-      {
-        code: "function My_function() {}",
-        errors: [
-          { message: 'Function name "My_function" should be in camelCase' },
-        ],
-      },
-    ],
-  },
-);
+ruleTester.run('camelcase-functions', customRules.rules['camelcase-functions'], {
+  valid: ['function validFunction() {}'],
+  invalid: [
+    {
+      code: 'function Invalid_Function() {}',
+      errors: [{ messageId: 'camelcase' }],
+      output: 'function invalidFunction() {}'
+    }
+  ]
+});
